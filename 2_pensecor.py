15723f6a6345d53c090734ff8a3477da62a62d26
felipeatorres006 <felipeatorres006@gmail.com>
2026-03-18 14:37:05 -0300

    Add color guessing game script

---
+++ 2_pensecor.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+os.system('clear')
+
+print('Seja bem vindo ao nosso jogo de advinhação de cores')
+input('Clique ENTER para inciar:')
+
+os.system('clear')
+
+print('Pense em uma cor nos próximos 3 segundos:')
+
+time.sleep(5)
+os.system('clear')
+
+print('A cor que você pensou')
+time.sleep(3)
+os.system('clear')
+
+print('Bem, deixa eu pensar um pouco...')
+time.sleep(5)
+os.system('clear')
+
+print(' A cor que você pensou foi azul ')
+time.sleep(4)
+os.system('clear')
+
+print(' Pera ai, na verdade foi o amarelo')
